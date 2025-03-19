02fd2dbd5d3b6bcf03e554c12ed4c39839d173fb
Oleganya123 <moonka321@mail.ru>
2025-03-19 19:50:50 +0300

    6part

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,12 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User findUserByCar(String model, int series) {
+      return sessionFactory.getCurrentSession()
+              .createQuery("FROM User u WHERE u.car.model = :model AND u.car.series = :series", User.class)
+              .setParameter("model", model)
+              .setParameter("series", series)
+              .uniqueResult();
+   }
 }
